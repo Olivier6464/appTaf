b08155298e4897c0520f5730b65a9cba1869c37b
Olivier Ernst <olivier.ernst0411@orange.fr>
2022-04-13 20:27:54 +0200

    shortcuts.js

--- JS/shorcuts.js
+++ JS/shorcuts.js
@@ -1,6 +1,7 @@
 function shorcuts() {
   document.addEventListener("keydown", (e) => {
     if (e.key.toLowerCase() === "b" && e.ctrlKey) {
+      e.preventDefault();
       listeToogle();
     } else if (e.key.toLowerCase() === "s" && e.ctrlKey) {
       e.preventDefault();
