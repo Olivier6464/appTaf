b184d6ac0f941360939e9883378d629e19abecae
Olivier Ernst <olivier.ernst0411@orange.fr>
2022-07-25 23:43:48 +0200

    shortcuts.js les suivis2

--- JS/shorcuts.js
+++ JS/shorcuts.js
@@ -11,8 +11,7 @@ function shorcuts() {
       toggleModalHelp();
     } else if (e.key.toLowerCase() === "p" && e.ctrlKey) {
       e.preventDefault();
-      window.location.href =
-        "https://olivier6464.github.io/appTaf/pages/suivis/index.html";
+      window.location.href = "/suivis";
       // affichage();
     }
     // else if (e.key === "F3") {
