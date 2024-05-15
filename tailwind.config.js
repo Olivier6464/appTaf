6b7a2dbe20f471c8b934bb23163a49c2a5c13e9c
olivier <olivier.ernst0411@orange.fr>
2024-05-15 16:57:17 +0200

    recherche acte

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'regal-blue': '#243c5a',
+                'bidass': '#bada55'
+            },
+        },
+    },
+    plugins: [],
+}
